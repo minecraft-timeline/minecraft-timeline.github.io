97f4be660366f49967dfffcb55e2fd96f8ee16a2
Henrique Colini <henriquecolini@gmail.com>
2026-01-08 16:43:04 -0300

    Added upcoming 26.1 versions

--- src/lib/editions/bedrock/upcomings.ts
+++ src/lib/editions/bedrock/upcomings.ts
@@ -2,21 +2,19 @@ import type { NaiveVersion } from "..";
 
 export const upcomings: NaiveVersion[] = [
   {
-    title: "Mounts of Mayhem",
-    subtitle: "1.21.X",
-    possibleDate: "Winter 2025",
+    title: "First Drop 2026",
+    subtitle: "26.1",
+    possibleDate: "Presumed Early 2026",
     type: "drop",
-    icon: "/java/version_mounts_of_mayhem.png",
-    funFact: "Did you know? The Zombie Horse was added in the game back in 2013 in java snapshot 13w16a.",
-    learnMore: "@Mounts_of_Mayhem",
+    icon: "/bedrock/version_26_1.png",
+    funFact: "Did you know? This is the first version of the game to use a two-digit year in its versioning system, marking a new era for Minecraft updates.",
+    learnMore: "@First_Drop_2026",
     mainFeatures: [
-      { text: "Nautilus" },
-      { text: "Spear" },
-      { text: "Zombie Nautilus" },
+      { text: "Unique new models for baby animals" },
+      { text: "Name Tags are now craftable" },
     ],
     minorFeatures: [
-      { text: "Breath of the Nautilus" },
-      { text: "Zombie Horse can now naturally spawn in survival" }
+      { text: "Clicking with a Spawn Egg on an animal spawns a baby" }
     ]
   }
 ] as const;
--- src/lib/editions/java/upcomings.ts
+++ src/lib/editions/java/upcomings.ts
@@ -2,6 +2,22 @@ import type { NaiveVersion } from "..";
 
 export const upcomings: NaiveVersion[] = [
   {
+    title: "First Drop 2026",
+    subtitle: "26.1",
+    possibleDate: "Presumed Early 2026",
+    type: "drop",
+    icon: "/java/version_26_1.png",
+    funFact: "Did you know? This is the first version of the game to use a two-digit year in its versioning system, marking a new era for Minecraft updates.",
+    learnMore: "@First_Drop_2026",
+    mainFeatures: [
+      { text: "Unique new models for baby animals" },
+      { text: "Name Tags are now craftable" },
+    ],
+    minorFeatures: [
+      { text: "Clicking with a Spawn Egg on an animal spawns a baby" }
+    ]
+  },
+  {
     title: "Vibrant Visuals",
     type: "minor",
     possibleDate: "Future",
